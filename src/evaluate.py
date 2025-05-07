d47c8a8e117b66340069b7e8cba790ed4c6cf0b1
cdondi <clivedondi@hotmail.com>
2025-05-06 20:04:00 -0500

    Cleaned up evaluate code. Changed dvc location in S3

--- src/evaluate.py
+++ src/evaluate.py
@@ -6,31 +6,31 @@ import os
 import mlflow
 from urllib.parse import urlparse
 
-
-os.environ['MLFLOW_TRACKING_URI']="https://dagshub.com/cdondi/machinelearningpipeline.mlflow"
-os.environ['MLFLOW_TRACKING_USERNAME']="cdondi"
-os.environ["MLFLOW_TRACKING_PASSWORD"]="f3168cea59becd1d7cc33ea645cbd7bbb725abd9"
-
+os.environ["MLFLOW_TRACKING_URI"] = "https://dagshub.com/cdondi/aiml_pima_diabetes_pipeline.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"] = "cdondi"
+os.environ["MLFLOW_TRACKING_PASSWORD"] = "f3168cea59becd1d7cc33ea645cbd7bbb725abd9"
 
 # Load parameters from params.yaml
 params = yaml.safe_load(open("params.yaml"))["train"]
 
-def evaluate(data_path,model_path):
-    data=pd.read_csv(data_path)
+
+def evaluate(data_path, model_path):
+    data = pd.read_csv(data_path)
     X = data.drop(columns=["Outcome"])
     y = data["Outcome"]
 
-    mlflow.set_tracking_uri("https://dagshub.com/cdondi/machinelearningpipeline.mlflow")
+    mlflow.set_tracking_uri(os.environ["MLFLOW_TRACKING_URI"])
 
     ## load the model from the disk
-    model=pickle.load(open(model_path,'rb'))
+    model = pickle.load(open(model_path, "rb"))
 
-    predictions=model.predict(X)
-    accuracy=accuracy_score(y,predictions)
+    predictions = model.predict(X)
+    accuracy = accuracy_score(y, predictions)
+
     ## log metrics to MLFLOW
-
-    mlflow.log_metric("accuracy",accuracy)
+    mlflow.log_metric("accuracy", accuracy)
     print("Model accuracy:{accuracy}")
 
-if __name__=="__main__":
-    evaluate(params["data"],params["model"])
+
+if __name__ == "__main__":
+    evaluate(params["data"], params["model"])
